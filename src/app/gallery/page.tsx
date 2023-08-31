f1fe60b3ca71b3be49118b6c07ae5b4ccbe885f6
JonathanYin <joyin@ucsd.edu>
2023-08-31 12:58:03 -0700

    Format image gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,15 +9,18 @@ type SearchResult = {
 export default async function GalleryPage() {
   const results = (await cloudinary.v2.search
     .expression("resource_type:image")
-    .sort_by("public_id", "desc")
-    .max_results(5)
+    .sort_by("created_at", "desc")
+    .max_results(10)
     .execute()) as { resources: SearchResult[] };
 
   return (
     <section>
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Gallery</h1>
-        <UploadButton />
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
+
         <div className="grid grid-cols-4 gap-4">
           {results.resources.map((result) => (
             <CloudImage

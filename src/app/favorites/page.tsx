96b88063f2fd9a35ac81b494ffb802bab28a6fb7
JonathanYin <joyin@ucsd.edu>
2023-08-31 16:33:40 -0700

    Init favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,34 +1,28 @@
-import UploadButton from "./uploadbutton";
 import cloudinary from "cloudinary";
-import { CloudImage } from "./cloudimage";
+import { CloudImage } from "../gallery/cloudimage";
+import { SearchResult } from "../gallery/page";
+import { ForceRefresh } from "@/components/forcerefresh";
 
-export type SearchResult = {
-  public_id: string;
-  tags: string[];
-};
-
-export default async function GalleryPage() {
+export default async function FavoritesPage() {
   const results = (await cloudinary.v2.search
-    .expression("resource_type:image")
+    .expression("resource_type:image AND tags=favorite")
     .with_field("tags")
     .sort_by("created_at", "desc")
     .max_results(20)
     .execute()) as { resources: SearchResult[] };
 
-  console.log(results);
-
   return (
     <section>
+      <ForceRefresh />
       <div className="flex flex-col gap-8">
         <div className="flex justify-between">
-          <h1 className="text-4xl font-bold">Gallery</h1>
-          <UploadButton />
+          <h1 className="text-4xl font-bold">Favorites</h1>
         </div>
 
         <div className="grid grid-cols-4 gap-4">
           {results.resources.map((result) => (
             <CloudImage
-              path="gallery"
+              path="favorites"
               key={result.public_id}
               imageData={result}
               width="400"
--- src/app/gallery/actions.tsx
+++ src/app/gallery/actions.tsx
@@ -3,12 +3,16 @@
 import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
 
-export async function MarkAsFavorite(publicId: string, isFavorite: boolean) {
+export async function MarkAsFavorite(
+  publicId: string,
+  isFavorite: boolean,
+  path: string
+) {
   if (isFavorite) {
     await cloudinary.v2.uploader.add_tag("favorite", [publicId]);
   } else {
     await cloudinary.v2.uploader.remove_tag("favorite", [publicId]);
   }
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  revalidatePath("/gallery");
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  revalidatePath(path);
 }

93bf459149ef3ab47023cd40c9ba13712b98b172
JonathanYin <joyin@ucsd.edu>
2023-09-03 14:01:13 -0700

    Init search tag form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Separator } from "@radix-ui/react-dropdown-menu";
 import { CldUploadButton } from "next-cloudinary";
 import { CldImage } from "next-cloudinary";
 import Image from "next/image";
@@ -13,10 +14,27 @@ export type UploadResult = {
 };
 
 export default function Home() {
-  const [imageId, setImageId] = useState("qbamwpfs81pwy2dgmdx6");
+  // const [imageId, setImageId] = useState("qbamwpfs81pwy2dgmdx6");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CldUploadButton
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+      <div className="space-y-1">
+        <h4 className="text-lg font-large leading-none">
+          Welcome to Cloud Frame
+        </h4>
+        <p className="text-md text-muted-foreground">
+          A simple, cloud-based photo library and editor
+        </p>
+      </div>
+      <Separator className="my-4" />
+      <div className="flex h-5 items-center space-x-4 text-sm">
+        <div>Blog</div>
+        <Separator orientation="vertical" />
+        <div>Docs</div>
+        <Separator orientation="vertical" />
+        <div>Source</div>
+      </div>
+      {/* <CldUploadButton
         onUpload={(result: UploadResult) => {
           setImageId(result.info.public_id);
         }}
@@ -31,7 +49,8 @@ export default function Home() {
           sizes="100vw"
           alt="Description of my image"
         />
-      )}
-    </main>
+      )} */}
+    </div>
+    // </main>
   );
 }

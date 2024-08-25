39406722afd59ff50d18aac3cfb9ae06e4b87e5a
JonathanYin <joyin@ucsd.edu>
2024-08-24 20:40:41 -0700

    Fix TS errors

--- src/app/gallery/uploadbutton.tsx
+++ src/app/gallery/uploadbutton.tsx
@@ -1,42 +1,31 @@
 "use client";
 
-import { CldUploadButton } from "next-cloudinary";
+import { CldUploadButton, CldUploadWidgetResults } from "next-cloudinary";
 import { UploadResult } from "../page";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function UploadButton() {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <Button asChild>
-      <CldUploadButton
-        onUpload={(result: UploadResult) => {
-          setTimeout(() => {
-            console.log("refreshed");
-            router.refresh();
-          }, 1000);
-        }}
-        uploadPreset="auttj4ku"
-      >
-        <div className="flex gap-2 items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 m-auto"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            />
-          </svg>
-          Upload
-        </div>
-      </CldUploadButton>
-    </Button>
-  );
+	return (
+		<Button asChild>
+			<CldUploadButton
+				onUpload={(result: CldUploadWidgetResults) => {
+					setTimeout(() => {
+						console.log("refreshed");
+						router.refresh();
+					}, 1000);
+				}}
+				uploadPreset="auttj4ku"
+			>
+				<div className="flex gap-2 items-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 m-auto">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+					</svg>
+					Upload
+				</div>
+			</CldUploadButton>
+		</Button>
+	);
 }

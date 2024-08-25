5215127c38b84137e0345ec7898d98fb5cbbeb3d
JonathanYin <joyin@ucsd.edu>
2024-08-24 20:36:46 -0700

    Fix TS errors

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,102 +1,62 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { CldImage } from "next-cloudinary";
+import { CldImage, CldImageProps } from "next-cloudinary";
 import { useState } from "react";
 
-export default function EditPage({
-  searchParams: { publicId },
-}: {
-  searchParams: { publicId: string };
-}) {
-  const [transformation, setTransformation] = useState<
-    undefined | "generative-fill" | "tint" | "blur" | "grayscale"
-  >();
+interface CustomCldImageProps extends CldImageProps {
+	tint?: string;
+	blur?: string;
+	grayscale?: boolean;
+}
 
-  return (
-    <section>
-      <div className="flex flex-col gap-8">
-        <div className="flex justify-between">
-          <h1 className="text-4xl font-bold">Edit {publicId}</h1>
-        </div>
-        <div className="flex gap-4">
-          <Button onClick={() => setTransformation(undefined)} variant="ghost">
-            Clear All
-          </Button>
+const CustomCldImage = (props: CustomCldImageProps) => {
+	return <CldImage {...props} />;
+};
 
-          <Button
-            onClick={() => setTransformation("generative-fill")}
-            variant="secondary"
-          >
-            Apply Generative Fill
-          </Button>
+export default function EditPage({ searchParams: { publicId } }: { searchParams: { publicId: string } }) {
+	const [transformation, setTransformation] = useState<undefined | "generative-fill" | "tint" | "blur" | "grayscale">();
 
-          <Button onClick={() => setTransformation("tint")} variant="secondary">
-            Apply Tint
-          </Button>
+	return (
+		<section>
+			<div className="flex flex-col gap-8">
+				<div className="flex justify-between">
+					<h1 className="text-4xl font-bold">Edit {publicId}</h1>
+				</div>
+				<div className="flex gap-4">
+					<Button onClick={() => setTransformation(undefined)} variant="ghost">
+						Clear All
+					</Button>
 
-          <Button onClick={() => setTransformation("blur")} variant="secondary">
-            Apply Blur
-          </Button>
+					<Button onClick={() => setTransformation("generative-fill")} variant="secondary">
+						Apply Generative Fill
+					</Button>
 
-          <Button
-            onClick={() => setTransformation("grayscale")}
-            variant="secondary"
-          >
-            Convert to Grayscale
-          </Button>
-        </div>
+					<Button onClick={() => setTransformation("tint")} variant="secondary">
+						Apply Tint
+					</Button>
 
-        <div className="grid grid-cols-2 gap-12">
-          <CldImage
-            src={publicId}
-            width="400"
-            height="300"
-            alt="Description of my image"
-          />
+					<Button onClick={() => setTransformation("blur")} variant="secondary">
+						Apply Blur
+					</Button>
 
-          {transformation === "generative-fill" && (
-            <CldImage
-              src={publicId}
-              width="400"
-              height="300"
-              alt="Description of my image"
-              crop="pad"
-              fillBackground
-            />
-          )}
+					<Button onClick={() => setTransformation("grayscale")} variant="secondary">
+						Convert to Grayscale
+					</Button>
+				</div>
 
-          {transformation === "tint" && (
-            <CldImage
-              src={publicId}
-              width="400"
-              height="300"
-              alt="Description of my image"
-              tint="equalize:80:blue:blueviolet"
-            />
-          )}
+				<div className="grid grid-cols-2 gap-12">
+					<CldImage src={publicId} width="400" height="300" alt="Description of my image" />
 
-          {transformation === "blur" && (
-            <CldImage
-              src={publicId}
-              width="400"
-              height="300"
-              alt="Description of my image"
-              blur="800"
-            />
-          )}
+					{transformation === "generative-fill" && <CldImage src={publicId} width="400" height="300" alt="Description of my image" crop="pad" fillBackground />}
 
-          {transformation === "grayscale" && (
-            <CldImage
-              src={publicId}
-              width="400"
-              height="300"
-              alt="Description of my image"
-              grayscale
-            />
-          )}
-        </div>
-      </div>
-    </section>
-  );
+					{transformation === "tint" && <CustomCldImage src={publicId} width="400" height="300" alt="Description of my image" tint="equalize:80:blue:blueviolet" />}
+
+					{transformation === "blur" && <CustomCldImage src={publicId} width="400" height="300" alt="Description of my image" blur="800" />}
+
+					{transformation === "grayscale" && <CustomCldImage src={publicId} width="400" height="300" alt="Description of my image" grayscale />}
+				</div>
+			</div>
+		</section>
+	);
 }
